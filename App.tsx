4c83c2dfc5dedc1031940a55863576ca9fd9047f
Rahulkirar098 <kirarrahul1996@gmail.com>
2025-05-13 09:21:00 +0530

    Ios working for fcm token

--- App.tsx
+++ App.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
-import {Navigation} from './src/navigation';
-import {Provider} from 'react-redux';
-import {PersistGate} from 'redux-persist/integration/react';
-import {persistor, store} from './src/redux';
-import {InternetStatus} from './src/modal';
+import { Navigation } from './src/navigation';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './src/redux';
+import { InternetStatus } from './src/modal';
 import { fcmToken } from './src/helper';
 
+import firebase from '@react-native-firebase/app';
+import messaging from '@react-native-firebase/messaging';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 function App(): React.JSX.Element {
-  fcmToken()
+
+  const aaaa = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+      fcmToken(); // now it's safe to fetch the token
+    }
+  }
+
+  aaaa()
+
   return (
     <PersistGate persistor={persistor}>
       <Provider store={store}>
--- src/helper/firebase.ts
+++ src/helper/firebase.ts
@@ -1,26 +1,21 @@
 import firebase from '@react-native-firebase/app';
 import messaging from '@react-native-firebase/messaging';
-
-
-// Store
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
-
-
-
-
 
 export const fcmToken = async () => {
     try {
-        //   let fcmToken = await AsyncStorage.getItem('fcmToken');
-        //   console.log('Old FCM Token:', fcmToken);
+        // Required for iOS: register for remote messages before getting token
+        await messaging().registerDeviceForRemoteMessages();
 
-        //   if (!fcmToken) {
+        // Get the FCM token
         let fcmToken = await messaging().getToken();
         console.log('New FCM Token:', fcmToken);
-        //     await AsyncStorage.setItem('fcmToken', fcmToken);
-        //   }
+
+        // Optionally store the token locally
+        await AsyncStorage.setItem('fcmToken', fcmToken);
     } catch (error) {
         console.log('Error retrieving or storing FCM Token:', error);
     }
 };
+
+
