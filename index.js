91022363dbbcd4dda2513fb7d1cb888f4823d009
Rahulkirar098 <kirarrahul1996@gmail.com>
2025-05-19 22:38:05 +0530

    sdk library install successfully

--- index.js
+++ index.js
@@ -5,6 +5,9 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { register } from "@videosdk.live/react-native-sdk";
+
+register();
 
 //
 import {register} from '@videosdk.live/react-native-sdk';
