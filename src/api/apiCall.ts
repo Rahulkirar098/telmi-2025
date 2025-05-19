e497fc504a8bb29e7820871f8e3725be0575c8e6
Rahulkirar098 <kirarrahul1996@gmail.com>
2025-05-19 22:38:05 +0530

    sdk library install successfully

--- src/api/apiCall.ts
+++ src/api/apiCall.ts
@@ -12,3 +12,20 @@ export const main = {
     return instance.post(endpoints.main.getSingleProfile, body)
   }
 }
+
+
+export const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiJkMjFkOGVjYy01YmJjLTRiZGUtYmE5OC0wZWU5MzIwMTYyMzkiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTc0NzY0MjA3MSwiZXhwIjoxNzQ4MjQ2ODcxfQ.P_7H_xuMWP_Q-yFZKTzGWFasPnuyxojBb14iha_m1Bc";
+
+export const createMeeting = async ({ token }:any) => {
+  const res = await fetch(`https://api.videosdk.live/v2/rooms`, {
+    method: "POST",
+    headers: {
+      authorization: `${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({}),
+  });
+
+  const { roomId } = await res.json();
+  return roomId;
+};
